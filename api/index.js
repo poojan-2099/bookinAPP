79316a76c56d01312a3ba697694b253b6a0ff38a
Patel <poojan-bharatbhai.patel.047@my.csun.edu>
2022-06-10 16:26:49 -0700

    Model scema for hotel added

---
+++ api/index.js
@@ -0,0 +1,36 @@
+import express  from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoute from "./routes/auth.js"
+import usersRoute from "./routes/users.js"
+import hotelsRoute from "./routes/hotels.js"
+import roomsRoute from "./routes/rooms.js"
+
+const app=express();
+dotenv.config();
+
+// connection to mongodb
+const connect = async ()=>{
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log("Monogodb ATlas Connected")
+      } catch (error) {
+        throw error;
+      }
+}
+
+mongoose.connection.on("disconnected", ()=>{
+    console.log("MongoDB Disconnected!")
+})
+
+//midddlewares
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
+
+   
+app.listen(8080, ()=>{
+    connect();
+    console.log("connected to backend")
+})
---
+++ api/routes/hotels.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const router= express.Router();
+
+//Create
+router.post("/",(req,res)=>{
+    
+})
+//update
+//delete
+//get
+//get all
+
+export default router;
