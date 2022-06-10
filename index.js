71f72431d4dd87f83907525532fb280115c173ec
Patel <poojan-bharatbhai.patel.047@my.csun.edu>
2022-06-09 19:19:05 -0700

    db connected

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 import express  from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+const app=express();
+dotenv.config();
+
+const connect = async ()=>{
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log("Monogodb ATlas Connected")
+      } catch (error) {
+        throw error;
+      }
+}
+   
+app.listen(8080, ()=>{
+    connect();
+    console.log("connected to backend")
+})
